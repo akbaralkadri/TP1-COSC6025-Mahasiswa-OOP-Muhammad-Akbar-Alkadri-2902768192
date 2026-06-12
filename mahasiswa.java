2cf9ad39df349ba86f6bd03d7ff54656e2366563
akbaralkadri <alkadri0508@gmail.com>
2026-06-12 15:21:09 +0700

    Add TP1 OOP Java code
    Add TP1 OOP Java code

---
+++ mahasiswa.java
@@ -0,0 +1,78 @@
+public class Mahasiswa {
+
+    private String nama;
+    private String nim;
+    private String jurusan;
+    private double ipk;
+
+    public Mahasiswa(String nama, String nim, String jurusan, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+        this.ipk = ipk;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public double getIpk() {
+        return ipk;
+    }
+
+    public void setIpk(double ipk) {
+        this.ipk = ipk;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println("=== Data Mahasiswa ===");
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jurusan: " + jurusan);
+        System.out.println("IPK: " + ipk);
+    }
+
+    public String cekKelulusan() {
+        if (ipk >= 3.00) {
+            return "Lulus";
+        } else {
+            return "Belum Lulus";
+        }
+    }
+
+    public void updateIpk(double ipkBaru) {
+        this.ipk = ipkBaru;
+    }
+
+    public String hitungPredikat() {
+        if (ipk >= 3.75) {
+            return "Dengan Pujian";
+        } else if (ipk >= 3.50) {
+            return "Sangat Memuaskan";
+        } else if (ipk >= 3.00) {
+            return "Memuaskan";
+        } else {
+            return "Perlu Perbaikan";
+        }
+    }
+}
